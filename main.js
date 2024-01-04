437ec36a1e732dd1064726ea7567054e0e67f61e
Alucardetat <alucardetat@gmail.com>
2024-01-04 10:08:55 -0500

    Updated

--- main.js
+++ main.js
@@ -37,9 +37,8 @@ function marry(person1, person2) {
   person1.married = true;
   person2.married = true;
 
-  person1.spouseName = person2.firstName + " " + person2.lastName;
-  person2.spouseName = person1.firstName + " " + person1.lastName;
-  return [person1, person2];
+  person1.spouseName = getFullName(person1);
+  person2.spouseName = getFullName(person2);
 }
 
 function divorce(person1, person2) {
